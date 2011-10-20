86f9c58a41c5e03002d360c3b75618fe47550f2b
Scott Elcomb <psema4@gmail.com>
2011-10-20 01:18:09 -0400

    fix stream assignment in echo example

--- prototype/scripts/atomos.js
+++ prototype/scripts/atomos.js
@@ -127,10 +127,9 @@ system.bin = system.bin || {};
 system.bin.echo = {
     exec: function(args) {
         // 'this' is the calling process, eg:
-        // console.log('process ' + this.name + ' executing);
-        var stdin =  (this.fd) ? this.fd[0] : false;
-        var stdout = (this.fd && this.fd.length > 1) ? this.fd[1] : false;
-        var stderr = (this.fd && this.fd.length > 2) ? this.fd[2] : false;
+        var stdin  = this.fd ? this.fd[0] : false;
+        var stdout = this.fd ? this.fd[1] : false;
+        var stderr = this.fd ? this.fd[2] : false;
 
         try {
             var message = (args instanceof Array) ? message = args.join(' ') : args;
